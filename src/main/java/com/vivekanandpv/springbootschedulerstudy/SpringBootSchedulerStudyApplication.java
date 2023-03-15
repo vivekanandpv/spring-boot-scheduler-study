3fdbf7106c25d5a352ba36ddad181fb01a5554b1
vivek <vivekanandpv@outlook.com>
2023-03-15 11:08:13 +0530

    Initial commit

---
+++ src/main/java/com/vivekanandpv/springbootschedulerstudy/SpringBootSchedulerStudyApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootschedulerstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulerStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootschedulerstudy/SpringBootSchedulerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootschedulerstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
