d2308cf5c0b510bb7730d5daa1c290b7d0f23b2c
vivek <vivekanandpv@outlook.com>
2023-03-15 11:31:33 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootschedulerstudy/scheduler/ScheduledJobRunner.java
@@ -0,0 +1,25 @@
+package com.vivekanandpv.springbootschedulerstudy.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ScheduledJobRunner {
+    private final Logger logger = LoggerFactory.getLogger(ScheduledJobRunner.class);
+
+    @Scheduled(fixedDelay = 2000)   //  every 2 seconds
+    public void doJob() {
+        logger.info(
+                String.format(
+                        "Thread %d is running scheduled job running at %s",
+                        Thread.currentThread().getId(),
+                        LocalDateTime.now()
+                )
+        );
+    }
+}
