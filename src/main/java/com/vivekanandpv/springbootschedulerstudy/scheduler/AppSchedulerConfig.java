607366b787d5b5d8b3c7438f3036c6993c631a61
vivek <vivekanandpv@outlook.com>
2023-03-15 11:12:50 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootschedulerstudy/scheduler/AppSchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.vivekanandpv.springbootschedulerstudy.scheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class AppSchedulerConfig {
+}
