da56cedd6c50be5379d182c4fe072b3ab7912b9c
vivek <vivekanandpv@outlook.com>
2023-03-15 11:37:30 +0530

    done

--- src/main/java/com/vivekanandpv/springbootschedulerstudy/scheduler/AppSchedulerConfig.java
+++ src/main/java/com/vivekanandpv/springbootschedulerstudy/scheduler/AppSchedulerConfig.java
@@ -1,9 +1,13 @@
 package com.vivekanandpv.springbootschedulerstudy.scheduler;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+//  configure the scheduler with an external property
+//  matchIfMissing is false by default
+@ConditionalOnProperty(name = "app.scheduler", matchIfMissing = false)
 public class AppSchedulerConfig {
 }
